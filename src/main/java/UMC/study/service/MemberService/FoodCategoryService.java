2f8858828a2865c5e6f73c34e042296a60f6d95d
설승환 <tjftmd7@gmail.com>
2024-06-03 15:31:43 +0900

    add FoodCategoryService

--- src/main/java/UMC/study/service/MemberService/FoodCategoryService.java
+++ src/main/java/UMC/study/service/MemberService/FoodCategoryService.java
@@ -1,2 +1,15 @@
-package UMC.study.service.MemberService;public class FoodCategoryService {
+package UMC.study.service.MemberService;
+
+import UMC.study.repository.FoodCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FoodCategoryService {
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    public boolean existsById(Long id) {
+        return foodCategoryRepository.existsById(id);
+    }
 }

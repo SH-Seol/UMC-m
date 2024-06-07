8dc181770a6a8e76dee158dd0db319e123c25f4f
설승환 <tjftmd7@gmail.com>
2024-06-08 01:09:25 +0900

    add Mission converter, controller

--- src/main/java/UMC/study/converter/MemberMissionConverter.java
+++ src/main/java/UMC/study/converter/MemberMissionConverter.java
@@ -1,2 +1,18 @@
-package UMC.study.converter;public class MemberMissionConverter {
+package UMC.study.converter;
+
+import UMC.study.domain.Member;
+import UMC.study.domain.Mission;
+import UMC.study.domain.enums.MissionStatus;
+import UMC.study.domain.mapping.MemberMission;
+import UMC.study.web._dto.MissionRequestDTO;
+
+
+public class MemberMissionConverter {
+    public static MemberMission toMemberMission(MissionRequestDTO.addMissionDTO request, Member member, Mission mission) {
+        return MemberMission.builder()
+                .member(member)
+                .mission(mission)
+                .status(MissionStatus.CHALLENGING)
+                .build();
+    }
 }
--- src/main/java/UMC/study/web/_controller/MissionRestController.java
+++ src/main/java/UMC/study/web/_controller/MissionRestController.java
@@ -1,2 +1,26 @@
-package UMC.study.web._controller;public class MissionRestController {
+package UMC.study.web._controller;
+
+import UMC.study.apiPayload.ApiResponse;
+import UMC.study.category.ExistMember;
+import UMC.study.service.MissionService.MissionCommandServiceImpl;
+import UMC.study.web._dto.MissionRequestDTO;
+import UMC.study.web._dto.MissionResponseDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MissionRestController {
+
+    private final MissionCommandServiceImpl missionCommandService;
+
+    @PostMapping("/{MemberId}/missions")
+    public ApiResponse<MissionResponseDTO.addMissionResultDTO> addMission(@RequestBody @Valid MissionRequestDTO.addMissionDTO request,
+                                                                          @ExistMember @PathVariable Long memberId){
+        MissionResponseDTO.addMissionResultDTO result = missionCommandService.addMissionToMember(memberId, request.getMissionId(), request);
+        return ApiResponse.onSuccess(result);
+    }
+
 }

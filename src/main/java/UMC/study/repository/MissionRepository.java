d6c5990290c3014fb690e9e3303a73d267962137
설승환 <tjftmd7@gmail.com>
2024-06-25 21:29:20 +0900

    add find method

--- src/main/java/UMC/study/repository/MissionRepository.java
+++ src/main/java/UMC/study/repository/MissionRepository.java
@@ -1,7 +1,11 @@
 package UMC.study.repository;
 
 import UMC.study.domain.Mission;
+import UMC.study.domain.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    Page<Mission> findMissionsByStore(Store store, PageRequest pageRequest);
 }
